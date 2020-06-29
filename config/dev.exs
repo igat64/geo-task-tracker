62710a41292571dac834901d7b29e5f08717430f
Igor Atroshkin <igor.atroshkin@gmail.com>
2020-06-29 23:39:38 +0300

    add "users" migration and schema

--- config/dev.exs
+++ config/dev.exs
@@ -2,8 +2,8 @@ use Mix.Config
 
 # Configure your database
 config :geo_task_tracker, GeoTaskTracker.Repo,
-  username: "postgres",
-  password: "postgres",
+  username: "igat",
+  password: "",
   database: "geo_task_tracker_dev",
   hostname: "localhost",
   show_sensitive_data_on_connection_error: true,
---
+++ lib/geo_task_tracker/user.ex
@@ -0,0 +1,18 @@
+defmodule GeoTaskTracker.User do
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  schema "users" do
+    field :name, :string
+    field :role, :string
+
+    timestamps()
+  end
+
+  @doc false
+  def changeset(user, attrs) do
+    user
+    |> cast(attrs, [:name, :role])
+    |> validate_required([:name, :role])
+  end
+end
